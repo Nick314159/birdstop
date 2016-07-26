2f2e509a2473457bf85977df3cc52f709424bebc
EC2 Default User <ec2-user@ip-172-31-35-27.us-west-2.compute.internal>
2016-07-26 04:48:10 +0000

    Initialized repo

---
+++ html/Brews/bird_stop.sql
@@ -0,0 +1,31 @@
+DROP SCHEMA IF EXISTS `bird_stop`;
+CREATE SCHEMA `bird_stop`;
+USE bird_stop;
+
+CREATE TABLE `beer_style`(
+`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(255) NOT NULL
+)ENGINE=INNODB DEFAULT CHARSET=UTF8 ROW_FORMAT=COMPACT;
+
+CREATE TABLE `brewery`(
+`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(255) NOT NULL
+)ENGINE=INNODB DEFAULT CHARSET=UTF8 ROW_FORMAT=COMPACT;
+
+CREATE TABLE `beer` (
+`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (255) NOT NULL,
+`brewery_id` INT(11) UNSIGNED NOT NULL,
+`origin` VARCHAR(255) NOT NULL,
+`beer_style_id` INT(11) UNSIGNED NOT NULL,
+`ABV` DOUBLE (3,2) NOT NULL,
+`IBU` INT(11),
+`price` DOUBLE(5,2),
+`on_tap` BOOLEAN,
+CONSTRAINT FOREIGN KEY (`beer_style_id`) REFERENCES `beer_style` (`id`),
+CONSTRAINT FOREIGN KEY (`brewery_id`) REFERENCES `brewery` (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8 ROW_FORMAT=COMPACT;
+
+INSERT INTO `bird_stop`.`beer_style` (`name`) VALUES ('Cider'),('Porter'),('Premium Biter/ESB'), ('Radler/Shand'), ('Imperial Stout'), ('American Pale Ale'), ('Pilsner');
+INSERT INTO `bird_stop`.`brewery` (`name`) VALUES ('Samual Adams'),('Wasatch Brewery'),('Firestone Walker Brewing'), ('Leinenkugel Brewing Company'), ('Mother Earth Brew Company'), ('Stone\'s'),('Goose Island Beer Company');
+INSERT INTO `bird_stop`.`beer` (`name`,`brewery_id`,`origin`,`beer_style_id`,`ABV`,`IBU`,`price`,`on_tap`) VALUES ('Angry Orchard','1','Cincinnati, OH','1','.05','0',4.50,'1'), ('Wasatch Polygamy','2','Park City, UT','2','.04','0',NULL,'1'), ('Firestone Walker Double Barrel Ale','3','Paso Robles, CA','3','.05','0',NULL,'1'), ('Leinenkugels Summer Shandy','4','Chippewa Falls, WI','4','.042','0',NULL,'1'),('Mother Earth Sintax Imperial Stout','5','Vista, CA','5','.081','0',5.50,'1'),('Stone Pale Ale 2.0','6','Greater London','6','.060','0',NULL,'1'), ('Goose Island Four Star Pils','7','Chicago, IL','7','.051','44',NULL,'1');
---
+++ html/Brews/brews.php
@@ -0,0 +1,127 @@
+<?php
+/* connect to the db */
+include('config.php');
+/* navigation bar */
+echo '<div style="width: 100%;">
+<div class="navigation-bar">
+  <ul>
+    <li><a href="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com/about.html">ABOUT</a></li>
+    <li><a href="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com/kitchen.html">KITCHEN<a/></li>
+    <li><a href="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com"><img class="logo" src="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com/pictures/favicon.jpg"></a></li>
+    <li><a href="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com/Events/events.php">EVENTS</a></li>
+    <li><a href="http://ec2-54-200-83-210.us-west-2.compute.amazonaws.com/Brews/brews.php">BREWS</a></li>
+  </ul>
+</div>
+</div>';
+/* Find all Beers on Tap */
+$sql = "SELECT id FROM beer WHERE on_tap=1 ORDER BY id";
+$result = mysqli_query($db,$sql);
+if(mysqli_num_rows($result)) {
+  /* Create Table */
+  echo '<table align="center" cellpadding="0" cellspacing="0" class="db-table">';
+  echo '<tr><th>Beer</th><th>Brewery</th><th>Origin</th><th>Beer Style</th><th>ABV</th><th>IBU</th><th>Price per Pint</th></tr>';
+  while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+    echo '<tr>';
+    $id = $row["id"];
+    /* Beer Name */
+    $sql = "SELECT name FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["name"],'</td>';
+    /* Brewery Name */
+    $sql = "SELECT brewery_id FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    $brewery_id = $row2["brewery_id"];
+    $sql = "SELECT name FROM brewery WHERE id='$brewery_id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["name"],'</td>';
+    /* Origin */
+    $sql = "SELECT origin FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["origin"],'</td>';
+    /* Beer Style */
+    $sql = "SELECT beer_style_id FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    $beer_style_id = $row2["beer_style_id"];
+    $sql = "SELECT name FROM beer_style WHERE id='$beer_style_id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["name"],'</td>';
+    /* ABV */
+    $sql = "SELECT ABV FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["ABV"],'</td>';
+    /* IBU */
+    $sql = "SELECT IBU FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["IBU"],'</td>';
+    /* price */
+    $sql = "SELECT price FROM beer WHERE id='$id'";
+    $result2 = mysqli_query($db,$sql);
+    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+    echo '<td>',$row2["price"],'</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
+}
+?>
+<html>
+
+<head>
+  <title>Brews</title>
+</head>
+<style>
+/* -- Logo -- */
+.logo {
+  float: center;
+  max-width: 80px;
+  max-height: 80px;
+  margin-bottom: -25px;
+}
+/* ~~ Top Navigation Bar ~~ */
+.navigation-bar {
+  height: 70px;
+  width: 100%;
+  margin-bottom: 1ex;
+}
+.navigation-bar ul {
+  padding: 0px;
+  margin: 0px;
+  text-align: center;
+}
+.navigation-bar li {
+  list-style-type: none;
+  padding: 0px;
+  height: 24px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  text-align: center;
+  display: inline;
+}
+.navigation-bar li a {
+  color: gray;
+  font-size: 16px;
+  font-family: 'Ubuntu', Helvetica, Arial, sans-serif;
+  text-decoration: none;
+  padding: 0px 0px 0px 10px;
+}
+/* Table */
+table.db-table {border-right:1px solid #ccc; border-bottom:1px solid #ccc; }
+table.db-table th { background:#eee; padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+table.db-table td { padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+<body>
+
+</body>
+
+</html>
