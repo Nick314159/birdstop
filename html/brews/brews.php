593b7ff0fe15ff4096cf3e56fa0876fc00f0b9c5
Thomas <came1788@gmail.com>
2016-07-27 18:33:15 -0600

    updated brews.php to correct relative reference to logo file

--- html/brews/brews.php
+++ html/brews/brews.php
@@ -86,11 +86,11 @@ table.db-table td {text-align: center; padding:5px; border-left:1px solid #ccc; 
 <div style="width: 100%;">
 <div class="navigation-bar">
   <ul>
-    <li><a href="about.html">ABOUT</a></li>
-    <li><a href="kitchen.html">KITCHEN<a/></li>
-    <li><a href="main.html"><img class="logo" src="pictures/favicon.jpg"></a></li>
-    <li><a href="events/events.php">EVENTS</a></li>
-    <li><a href="brews/brews.php">BREWS</a></li>
+    <li><a href="../about.html">ABOUT</a></li>
+    <li><a href="../kitchen.html">KITCHEN<a/></li>
+    <li><a href="../main.html"><img class="logo" src="..pictures/favicon.jpg"></a></li>
+    <li><a href="../events/events.php">EVENTS</a></li>
+    <li><a href="../brews/brews.php">BREWS</a></li>
   </ul>
 </div>
 </div>
