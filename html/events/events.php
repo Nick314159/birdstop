81b01e32425100537502ee808f9e3aa303f9f15a
Thomas <came1788@gmail.com>
2016-07-29 14:35:32 -0600

    blah

--- html/events/events.php
+++ html/events/events.php
@@ -15,7 +15,7 @@
     <li><a href="../about.html">ABOUT</a></li>
     <li><a href="../kitchen.html">KITCHEN<a/></li>
     <li><a href="../main.html"><img class="logo" src="../pictures/logo.png"></a></li>
-    <li><a href="events.php">EVENTS</a></li>
+    <li><a href="../events/events.php">EVENTS</a></li>
     <li><a href="../brews/brews.php">BREWS</a></li>
   </ul>
 </div>
