2ebf985d70a1e03dafe6a93702d30f0aacda3f1a
Thomas <came1788@gmail.com>
2016-07-29 14:30:07 -0600

    removed uneccesary style items from brews and events

--- html/events/events.php
+++ html/events/events.php
@@ -6,46 +6,7 @@
 <head>
   <title>Events</title>
 </head>
-<style>
-/* -- Logo -- */
-.logo {
-  float: center;
-  max-width: 80px;
-  max-height: 80px;
-  margin-bottom: -25px;
-}
-/* ~~ Top Navigation Bar ~~ */
-.navigation-bar {
-  height: 70px;
-  width: 100%;
-  margin-bottom: 1ex;
-}
-.navigation-bar ul {
-  padding: 0px;
-  margin: 0px;
-  text-align: center;
-}
-.navigation-bar li {
-  list-style-type: none;
-  padding: 0px;
-  height: 24px;
-  margin-top: 4px;
-  margin-bottom: 4px;
-  text-align: center;
-  display: inline;
-}
-.navigation-bar li a {
-  color: gray;
-  font-size: 16px;
-  font-family: 'Ubuntu', Helvetica, Arial, sans-serif;
-  text-decoration: none;
-  padding: 0px 0px 0px 10px;
-}
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-</style>
+<link rel='stylesheet' type='text/css' href='../stylesheet.css'/>
 <body>
 <!-- Navigation Bar -->
 <div style="width: 100%;">
