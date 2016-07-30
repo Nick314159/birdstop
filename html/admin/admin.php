70556ee5e269d2d790f1e32a9bd57eb56c2dbbc0
Thomas <came1788@gmail.com>
2016-07-30 00:43:36 -0600

    updated admin.php

--- html/admin/admin.php
+++ html/admin/admin.php
@@ -31,25 +31,25 @@ include_once "../config.php";
     $result = mysqli_query($db, "SELECT b.id, b.name, br.name as brewery, b.origin, bs.name as beer_style, b.ABV, b.IBU, b.price, b.on_tap FROM beer b JOIN brewery br on b.brewery_id = br.id JOIN beer_style bs ON b.beer_style_id = bs.id ORDER BY b.on_tap DESC, b.name");
     echo '<table align="center" cellpadding="0" cellspacing="0" class="db-table"><tr><th></th><th>Name</th> <th>Brewery</th><th>Origin</th><th>Beer Style</th><th>ABV</th><th>IBV</th> <th>Price</th><th>On Tap</th></tr>';
     while ($row = mysqli_fetch_array($result)) {
-      echo "<tr row" . $row['id'] . ">";
+      echo '<tr>' . $row['id'] . '</tr>';
       echo "<td><button type='button' onClick=\"Javascript:window.location.href = 'editBeer.php?rowId=" . $row['id'] . "';\">Edit</button></td>";
-      echo "<td>" . $row['name'] . "</td>";
-      echo "<td>" . $row['brewery'] . "</td>";
-      echo "<td>" . $row['origin'] . "</td>";
-      echo "<td>" . $row['beer_style'] . "</td>";
-      echo "<td>" . $row['ABV'] . "</td>";
-      echo "<td>" . $row['IBU'] . "</td>";
-      echo "<td>" . $row['price'] . "</td>";
-      echo "<td> <input name='on_tap' type='checkbox'  disabled='disabled'";
+      echo '<td>' . $row['name'] . '</td>';
+      echo '<td>' . $row['brewery'] . '</td>';
+      echo '<td>' . $row['origin'] . '</td>';
+      echo '<td>' . $row['beer_style'] . '</td>';
+      echo '<td>' . $row['ABV'] . '</td>';
+      echo '<td>' . $row['IBU'] . '</td>';
+      echo '<td>' . $row['price'] . '</td>';
+      echo '<td> <input name="on_tap" type="checkbox"  disabled="disabled"';
       if ($row['on_tap'] == 1) {
         echo " value ='1' checked";
       } 
       else {
-        echo " value ='0'";
+        echo ' value ="0"';
       }
-      echo "></td></tr>";
+      echo '></td></tr>';
     }
-    echo "</table>";
+    echo '</table>';
     ?>
 </div>
 </body>
