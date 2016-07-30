b6ff56d42b33fcfd52a81db0b8cc13593f5bedcf
Thomas <came1788@gmail.com>
2016-07-30 12:18:49 -0600

    updated admin.php

--- html/admin/admin.php
+++ html/admin/admin.php
@@ -1,5 +1,30 @@
 <?php
 include_once "../config.php";
+$result = mysqli_query($db, "SELECT b.id, b.name, br.name as brewery, b.origin, bs.name as beer_style, b.ABV, b.IBU, b.price, b.on_tap FROM beer b JOIN brewery br on b.brewery_id = br.id JOIN beer_style bs ON b.beer_style_id = bs.id ORDER BY b.on_tap DESC, b.name");
+$table .= '<table align="center" cellpadding="0" cellspacing="0" class="db-table"><tr><th></th><th>Name</th> <th>Brewery</th><th>Origin</th><th>Beer Style</th><th>ABV</th><th>IBV</th> <th>Price</th><th>On Tap</th></tr>';
+while($row = mysqli_fetch_array($result)) {
+  $table .= "<td><button type='button' onClick=\"Javascript:window.location.href = 'editBeer.php?rowId=" . $row['id'] . "';\">Edit</button></td>";
+  $table .= '<td>' . $row['name'] . '</td>';
+  $table .= '<td>' . $row['brewery'] . '</td>';
+  $table .= '<td>' . $row['origin'] . '</td>';
+  $table .= '<td>' . $row['beer_style'] . '</td>';
+  $table .= '<td>' . $row['ABV'] . '</td>';
+  $table .= '<td>' . $row['IBU'] . '</td>';
+  $table .= '<td>' . $row['price'] . '</td>';
+  $table .= '<td> <input name="on_tap" type="checkbox"  disabled="disabled"';
+  if($row['on_tap'] ==1 ) {
+    $table .= 'value="1" checked';
+  }
+  else {
+    $table .= 'value="0"';
+  }
+  $table .= '></td></tr>';
+}
+$table .= '</table>';
+//New Beer Button
+if($_POST['beerNew']) {
+  header("location: editBeer.php");
+}
 ?>
 <html>
 <!DOCTYPE HTML>
@@ -17,30 +42,12 @@ include_once "../config.php";
 </div>
 <!-- Table -->
 <div style="width: 100%">
-  <button type='button' onClick="Javascript:window.location.href = 'editBeer.php'">Add New Beer</button>
-    <?php
-    $result = mysqli_query($db, "SELECT b.id, b.name, br.name as brewery, b.origin, bs.name as beer_style, b.ABV, b.IBU, b.price, b.on_tap FROM beer b JOIN brewery br on b.brewery_id = br.id JOIN beer_style bs ON b.beer_style_id = bs.id ORDER BY b.on_tap DESC, b.name");
-    echo '<table align="center" cellpadding="0" cellspacing="0" class="db-table"><tr><th></th><th>Name</th> <th>Brewery</th><th>Origin</th><th>Beer Style</th><th>ABV</th><th>IBV</th> <th>Price</th><th>On Tap</th></tr>';
-    while ($row = mysqli_fetch_array($result)) {
-      echo "<td><button type='button' onClick=\"Javascript:window.location.href = 'editBeer.php?rowId=" . $row['id'] . "';\">Edit</button></td>";
-      echo '<td>' . $row['name'] . '</td>';
-      echo '<td>' . $row['brewery'] . '</td>';
-      echo '<td>' . $row['origin'] . '</td>';
-      echo '<td>' . $row['beer_style'] . '</td>';
-      echo '<td>' . $row['ABV'] . '</td>';
-      echo '<td>' . $row['IBU'] . '</td>';
-      echo '<td>' . $row['price'] . '</td>';
-      echo '<td> <input name="on_tap" type="checkbox"  disabled="disabled"';
-      if ($row['on_tap'] == 1) {
-        echo " value ='1' checked";
-      } 
-      else {
-        echo ' value ="0"';
-      }
-      echo '></td></tr>';
-    }
-    echo '</table>';
-    ?>
+  <div class="margin">
+    <form method="post">
+      <input type="submit" name="beerNew" value="Add New Beer"><br><br>
+    </form>
+  </div>
+  <?php echo $table; ?>
 </div>
 </body>
 
