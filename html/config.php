4dcb0d3189944df18b0ffa3cd15a5e64433f1744
root <came1788@gmail.com>
2017-02-10 17:20:49 -0700

    updated config

--- html/config.php
+++ html/config.php
@@ -2,8 +2,8 @@
 define('DB_SERVER', '127.0.0.1');
 //define('DB_USERNAME', 'root');
 //define('DB_PASSWORD', 'blue#8315$17');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'blue#8315$17');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'Server#1707');
 define('DB_DATABASE', 'bird_stop');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
